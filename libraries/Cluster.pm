e63e181b0632bb267b1010d21064ba168513a366
Eduardo Eyras <eduardo.eyras@upf.edu>
2018-03-16 12:25:51 +0100

    updated documentation

--- libraries/Cluster.pm
+++ libraries/Cluster.pm
@@ -2,18 +2,36 @@
 
 use strict;
 
+
+=pod
+ 
+=head1 DESCRIPTION
+ 
+This module contains a function to cluster ranges. 
+
+=head2 function cluster()
+
+The funcion cluster reads a pointer to a list (arrayref) 
+of pointers to lists (arrayrfs). Each of those lists will hold the information 
+about the ranges (e.g. exons, gene loci, transcript genomic extensions). 
+After sorting the ranges, the clustering is performed linearly in the number of elements O(n).
+Additionally it reads the positions in the element lists of the start and end
+to be used for the clustering. This function is agnostic of reference (chromosome)
+and strand (genome). These issues must be sorted out outside the function.
+ 
+=cut
+
 sub cluster{
     my ($loci, $s, $e, $verbose) = @_;
     
     # $loci is an arrayref to arrayrefs
-    my @loci = @$loci;
     
     # we generate cluster of loci
     my $locus_clusters;
         
     # sort loci by start coordinate in ascending order
     # each loci is of the form  [$locus_chr,$locus_start,$locus_end,$locus_id]
-    my @sorted_loci = sort { $a->[$s] <=> $b->[$s] } @loci;
+    my @sorted_loci = sort { $a->[$s] <=> $b->[$s] } @$loci;
 
     # start and end coordinates of the clusters
     my @cluster_start;
@@ -37,7 +55,6 @@ sub cluster{
             $count++;
             if ($verbose){
                 print  "cluster_count: $cluster_count\n";
-                print  "cluster_start: $cluster_start[$cluster_count] cluster_end: $cluster_end[$cluster_count]\n";
                 print  "cluster_start: $cluster_start[$cluster_count] cluster_end: $cluster_end[$cluster_count]\n";
             }
             next LOCUS;
