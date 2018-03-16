8162fb362b8157c7ee05e9d269a7b66134377408
Eduardo Eyras <eduardo.eyras@upf.edu>
2018-03-16 12:51:44 +0100

    added FileReader

---
+++ libraries/FileReader.pm
@@ -0,0 +1,67 @@
+package FileReader;
+
+use strict;
+
+sub read_file{
+    my ($file, $type, $v) = @_;
+    if ($type eq "GTF"){
+	return read_GTF($file, $v);
+    }
+    elsif( $type eq "GFF"){
+	return read_GFF($file, $v);
+    }
+}
+
+
+sub read_GTF{
+    my ($file, $verbose) = @_;
+
+    my $trans;
+    open(IN,"<$file") or die("cannot open $file");
+    while(<IN>){
+	chomp;
+	#Chr1   TAIR10  exon    3631    3913    .       +       .       ...
+	my ($chr, $db, $feature, $start, $end, $score, $strand, $frame, $col9) = split /\t/, $_;
+	next unless $feature eq "exon";
+	my ($t_id, $g_id);
+	if ( $col9=~/gene_id\s+\"(\S+)\";\s+transcript_id\s+\"(\S+)\";/){
+	    $t_id = $1;
+	    $g_id = $2;
+	}
+	my $exon = [$chr, $start, $end, $strand, $t_id, $g_id];
+	print "storing exon: $chr, $start, $end, $strand, $t_id, $g_id\n" if $verbose;
+	push( @{$trans->{$t_id}}, $exon );
+    }
+    return $trans;
+}
+
+
+
+sub read_GFF{
+    my ($file, $verbose) = @_;
+
+    my $trans;
+    open(IN,"<$file") or die("cannot open $file");
+    while(<IN>){
+	chomp;
+	#Chr1   TAIR10  exon    3631    3913    .       +       .       Parent=AT1G01010.1
+	my ($chr, $db, $feature, $start, $end, $score, $strand, $frame, $col9) = split /\t/, $_;
+	next unless $feature eq "exon";
+	my ($t_id, $g_id);
+	if ($col9 =~/Parent=(\S+)/){
+	    $t_id = $1;
+	    $g_id = $t_id;
+	}    
+	else{
+	    $t_id = $col9;
+	    $g_id = $t_id;
+	}
+	my $exon = [$chr, $start, $end, $strand, $t_id, $g_id];
+	print "storing exon: $chr, $start, $end, $strand, $t_id, $g_id\n" if $verbose;
+	push( @{$trans->{$t_id}}, $exon );
+    }
+    return $trans;
+}
+
+
+1;
