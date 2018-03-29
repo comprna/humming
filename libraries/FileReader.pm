dc69a6f89f15115db407e8a7aef21cbe73161a05
Eduardo Eyras <eduardo.eyras@upf.edu>
2018-03-29 17:31:48 +0200

    now able to read BED12

--- libraries/FileReader.pm
+++ libraries/FileReader.pm
@@ -10,8 +10,26 @@ sub read_file{
     elsif( $type eq "GFF"){
 	return read_GFF($file, $v);
     }
+    elsif( $type eq "BED12"){
+	return read_BD12($file, $v);
+    }
+    elsif( $type eq "PAF"){
+	return read_PAF($file, $v);
+    }
 }
 
+
+# GTF
+#
+# 1 chr
+# 2 DB
+# 3 feature
+# 4 start (1-based)
+# 5 end
+# 6 score
+# 7 strand
+# 8 frame
+# 9 gene_id "<id>"; transcript_id "<id>";
 
 sub read_GTF{
     my ($file, $verbose) = @_;
@@ -43,8 +61,17 @@ sub read_GTF{
     return \@transcripts;
 }
 
-
-
+# GFF
+#
+# 1 chr
+# 2 DB
+# 3 feature
+# 4 start (1-based)
+# 5 end
+# 6 score
+# 7 strand
+# 8 frame
+# 9 group ID / Parent=ID.
 sub read_GFF{
     my ($file, $verbose) = @_;
 
@@ -77,5 +104,72 @@ sub read_GFF{
     return \@transcripts;
 }
 
+
+# BED12
+#
+# 1  chr
+# 2  chr_start (0 based)
+# 3  chr_end
+# 4  name
+# 5  score
+# 6  strand
+# 7  thick_start
+# 8  thick_end
+# 9  ItemGBgroup ID / Parent=ID.
+# 10 block_count
+# 11 block_sizes
+# 12 block_starts (relative to chr_start)
+sub read_BED12{
+    my ($file, $verbose) = @_;
+
+    my $trans;
+    my %count;
+    open(IN,"<$file") or die("cannot open $file");
+    while(<IN>){
+        chomp;
+	my ($chr, $chr_start, $chr_end, $name, $score, $strand, $thick_start, $thick_end, $item, 
+	    $block_count, $block_sizes, $block_starts) = split /\s+/, $_;
+	
+        # In case names are duplicated with add a counter "_2, _3..."
+	$count{$name}++;
+	my $t_id = $name;
+	if ( $count{$name} > 1 ){
+	    $t_id .= "_".$count{$name};
+	}
+	my $g_id = $t_id;
+	
+	my @start = split "\,", $block_starts;
+	my @size = split "\,", $block_sizes;
+	
+	$chr_start++;
+	for(my $i=0; $i<$block_count; $i++){
+	    my ($exon_start, $exon_end);
+	    $exon_start = $chr_start + $start[$i];
+	    $exon_end   = $exon_start + $size[$i] - 1;
+	    
+	    my $exon = [$chr, $exon_start, $exon_end, $strand, $t_id, $g_id];
+	    print "storing exon: $chr, $exon_start, $exon_end, $strand, $t_id, $g_id\n" if $verbose;
+	    push( @{$trans->{$t_id}}, $exon );
+	}
+    }
+    my @transcripts;
+    foreach my $id (keys %$trans ){
+	my @e = @{$trans->{$id}};
+	my $t = \@e;
+	push( @transcripts, $t);
+    }
+    return \@transcripts;
+}
+
+# PAF
+sub read_PAF{
+    my ($file, $verbose) = @_;
+
+    my $trans;
+    my %count;
+    
+    print STDERR "PAF reader not implemented yet. Apologies.\n";
+    exit(0)
+}
 
 1;
