d7a84c6c3e553b1dbdea1258020821437cae66e2
Eduardo Eyras <eduardo.eyras@upf.edu>
2018-03-29 17:46:04 +0200

    adding writing options or GFF, GTF

--- libraries/FileWriter.pm
+++ libraries/FileWriter.pm
@@ -2,7 +2,63 @@ package FileWriter;
 
 use strict;
 
+
+sub write_file{
+    my ($trans, $format, $output) = @_;
+
+    if ($format eq "GTF"){
+        return write_GTF($trans, $output);
+    }
+    elsif( $format eq "GFF"){
+        return write_GFF($trans, $output);
+    }
+    elsif( $format eq "BED12"){
+        return write_BD12($trans, $output);
+    }
+    elsif( $format eq "PAF"){
+        return write_PAF($trans, $output);
+    }
+}
+
+
 sub write_GTF{
+    my ($trans, $output) = @_;
+    
+    open(OUT,">$output") or die("cannot open file $output for writing");
+    foreach my $t (@$trans){
+	foreach my $e (get_sorted_exons($t)){
+	    my ($chr, $start, $end, $strand, $t_id, $g_id) = @$e;
+	    my $source  = "humming";
+	    my $feature = "exon";
+	    my $frame   = ".";
+	    my $score   = 100;
+	    my $col9    = "gene_id \"".$g_id."\"; transcript_id \"".$t_id."\";";
+	    my $s = join "\t", ($chr, $source, $feature, $start, $end, $score, $strand, $frame, $col9);
+	    print OUT $s."\n";
+	}
+    }
+}
+
+
+sub write_GFF{
+    my ($trans, $output) = @_;
+
+    open(OUT,">$output") or die("cannot open file $output for writing");
+    foreach my $t (@$trans){
+	foreach my $e (get_sorted_exons($t)){
+	    my ($chr, $start, $end, $strand, $t_id, $g_id) = @$e;
+	    my $source  = "humming";
+	    my $feature = "exon";
+	    my $frame   = ".";
+	    my $score   = 100;
+	    my $col9    = $t_id;
+	    my $s = join "\t", ($chr, $source, $feature, $start, $end, $score, $strand, $frame, $col9);
+	    print OUT $s."\n";
+	}
+    }
+}
+
+sub write_genes_in_GTF{
     my ($genes, $label, $output) = @_;
 
     open(OUT,">$output") or die("cannot open file $output for writing");
