dacfb49e1a34abd1a5d7a4d9b5dcc77d6eb2f418
Eduardo Eyras <eduardo.eyras@upf.edu>
2018-04-07 17:08:38 +0200

    added option to modify the deletion size to find introns in cigar string

--- convert.pl
+++ convert.pl
@@ -6,10 +6,13 @@ use libraries::FileReader;
 use libraries::FileWriter;
 use Getopt::Long;
 
-my ($file, $format_in, $output, $format_out, $help, $distance, $verbose);
+my ($file, $format_in, $output, $format_out, $help, $distance, $verbose, $del_size);
 
 # default distance between matches in PAF file to join them into a transcript
 $distance = 100000;
+
+# default size of a deletion in the cigar string to be considered a potential intron
+$del_size = 25;
 
 # check options
 my $result = GetOptions(
@@ -18,6 +21,7 @@ my $result = GetOptions(
     'o|output=s'     => \$output,
     'u|format_out=s' => \$format_out,
     'd|distance=i'   => \$distance,
+    's|del_size=i'   => \$del_size,
     'v|verbose'      => \$verbose,
     'h|help'         => \$help
     );
@@ -45,7 +49,8 @@ sub print_usage{
     print STDERR "-f | --format_in:  Format of the transcript file: GTF, GFF, BED12, PAF\n";
     print STDERR "-o | --output:     Output file name\n";
     print STDERR "-u | --format_out: Format of the transcript file: GTF, GFF, BED12, PAF\n";
-    print STDERR "-d | --distance:   Distance between matches in PAF file to join them into a transcript (default: 100000)\n";
+    print STDERR "-d | --distance:   Distance between matches in cigar string file to join them into a transcript (default: 100000)\n";
+    print STDERR "-s | --del_size:   Smallest size of a deletion in the cigar string to be considered a possible intron (detault: 25)\n"; 
     print STDERR "-v | --verbose:    Switch on useful prints for debugging\n";
     print STDERR "-h | --help:       Print this help\n";
     print STDERR "\n";
@@ -54,7 +59,7 @@ sub print_usage{
 
 
 # read input
-my $trans = FileReader::read_file($file, $format_in, $verbose, $distance);
+my $trans = FileReader::read_file($file, $format_in, $verbose, $distance, $del_size);
 
 # write output
 FileWriter::write_file($trans, $output, $format_out);
