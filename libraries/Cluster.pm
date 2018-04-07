3ab030e7c95609c242894d5981fba2b916872a3d
Eduardo Eyras <eduardo.eyras@upf.edu>
2018-04-07 18:28:18 +0200

    added some more info into cluster_by_proximity()

--- libraries/Cluster.pm
+++ libraries/Cluster.pm
@@ -23,7 +23,6 @@ sub cluster{
     my $locus_clusters;
         
     # sort loci by start coordinate in ascending order
-    # each loci is of the form  [$locus_chr,$locus_start,$locus_end,$locus_id]
     my @sorted_loci = sort { $a->[$s] <=> $b->[$s] } @$loci;
 
     # start and end coordinates of the clusters
@@ -179,6 +178,95 @@ sub cluster_transcripts_2sets{
     }
     my ( $clusters, $cluster_start, $cluster_end) = cluster_transcripts($trans);
     return ($clusters, $label);
+}
+###########################################
+#
+# This method cluster ranges by coordinate proximity
+#
+sub cluster_by_proximity{
+    my ($loci, $s, $e, $distance, $verbose) = @_;
+    
+    # $loci is an arrayref to arrayrefs
+    
+    # we generate cluster of loci
+    my $locus_clusters;
+        
+    # sort loci by start coordinate in ascending order
+    my @sorted_loci = sort { $a->[$s] <=> $b->[$s] } @$loci;
+    
+    if ($verbose){
+	foreach my $l (@sorted_loci){
+	    print "reading: @$l\n";
+	}
+    }
+
+    # start and end coordinates of the clusters
+    my @cluster_start;
+    my @cluster_end;
+    
+    # Create the first locus_cluster with the first locus
+    my $locus_cluster = [];
+    push( @$locus_clusters, $locus_cluster);
+    
+    # we go over all of them in sorted order (from left to right)
+    my $cluster_count = 0;
+    my $count = 0;    
+
+    LOCUS:
+    foreach my $locus ( @sorted_loci ){
+        if ($count == 0){
+            # Create the first locus_cluster with the first locus
+            push( @$locus_cluster, $locus);
+            $cluster_start[0]  =  $locus->[$s];
+            $cluster_end[0]    =  $locus->[$e];
+            $count++;
+            if ($verbose){
+                print  "cluster_count: $cluster_count\n";
+                print  "cluster_start: $cluster_start[$cluster_count] cluster_end: $cluster_end[$cluster_count]\n";
+            }
+            next LOCUS;
+        }
+        if ($verbose){
+	    print "testing range: ". $locus->[$s]. " - ". $locus->[$e]."\n";
+	    print "cluster_count: $cluster_count\n";
+            print "cluster_start: $cluster_start[$cluster_count] cluster_end: $cluster_end[$cluster_count]\n";
+	    print "distance: ".($locus->[$s] - $cluster_end[$cluster_count])."\n";
+        }
+	# test proximity
+	#     s  e                                         s  e
+	#     ####       cs      ce         cs     ce      ####
+	#         <----->##########         #########<---->
+        #if ( $cluster_start[$cluster_count] - $locus->[$e] <= $distance ||  $locus->[$s] - $cluster_end[$cluster_count] < $distance ){
+	if ( abs($locus->[$s] - $cluster_end[$cluster_count]) < $distance ){
+
+            # add locus to current cluster
+            print "added\n" if $verbose;
+            push( @$locus_cluster, $locus );
+            
+            # update start and end of cluster if necessary
+            if ($locus->[$s] < $cluster_start[$cluster_count]) {
+                $cluster_start[$cluster_count] = $locus->[$s];
+            }
+            if ($locus->[$e] > $cluster_end[$cluster_count]) {
+                $cluster_end[$cluster_count]   = $locus->[$e];
+            }
+        }
+        else{
+            # if there is no overlap, proceed with the next one:
+            # create new cluster (same variable name, new memory address!!!!)
+            $locus_cluster = [];
+            push( @$locus_clusters, $locus_cluster);  # store ref first, fill later
+	    
+            # add locus in new cluster
+            push( @$locus_cluster, $locus );
+            $cluster_count++;
+            $cluster_start[$cluster_count] = $locus->[$s];
+            $cluster_end[$cluster_count]   = $locus->[$e];
+        }
+    }
+    # returns an arrayref of clusters (each cluster is an arrayref of the same elements as in @loci)
+    # and the arrayrefs to the start and end of each cluster
+    return( $locus_clusters, \@cluster_start, \@cluster_end);
 }
 
 ###########################################
